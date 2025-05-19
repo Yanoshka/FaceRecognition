e0ba78de8f12ec05accef48663d63edef1434f4e
Яна <144015493+Yanoshka@users.noreply.github.com>
2025-05-19 23:57:10 +0300

    add .env usage

--- Inmoov head/app voice beta/openai_api.py
+++ Inmoov head/app voice beta/openai_api.py
@@ -1,14 +1,21 @@
 from openai import OpenAI
+import os
+from dotenv import load_dotenv
 
-api_key = ""
+load_dotenv()
+
+MY_ENV_VAR = os.getenv('OPENAI_API_KEY')
+
+api_key = MY_ENV_VAR
 client = OpenAI(api_key=api_key)
 
-def query_api(user_input):
 
+def query_api(user_input):
     completion = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {"role": "system", "content": """You are a helpful assistant. Your name is Inmoov. You are a robot. Do not provide long responses. Answer with 1-3 sentences"""},
+            {"role": "system",
+             "content": """You are a helpful assistant. Your name is Inmoov. You are a robot. Do not provide long responses. Answer with 1-3 sentences"""},
             {
                 "role": "user",
                 "content": user_input
@@ -17,8 +24,3 @@ def query_api(user_input):
     )
 
     return completion.choices[0].message.content
-
-
-
-
-
